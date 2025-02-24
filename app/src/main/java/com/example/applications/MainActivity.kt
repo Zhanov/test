169fadf992750e185d815a3981d7929f06102bc0
main <zheksinbi@mail.ru>
2025-02-24 18:46:14 +0500

    calculator

--- app/src/main/java/com/example/applications/MainActivity.kt
+++ app/src/main/java/com/example/applications/MainActivity.kt
@@ -16,18 +16,37 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        var flag = false
-        binding.btn.setOnClickListener {
-            if (!flag) {
-                binding.tvText.text = "Salem Alem"
-                flag = true
-            } else {
-                binding.tvText.text = "Hello World"
-                flag = false
+
+
+        binding.button.setOnClickListener {
+            var editText1 = binding.editTextNumber1.text.toString()
+            var editText2 = binding.editTextNumber2.text.toString()
+            var editTextSymbol = binding.editTextSymbol.text.toString()
+            var res = 0
+            if (editText1 == null || editText2 == null || editTextSymbol == null) {
+                binding.tvResult.text = "write number"
+            }
+            else {
+                if (editTextSymbol == "1") {
+                    res = editText1.toInt() + editText2.toInt()
+                    binding.tvResult.text = res.toString()
+                }
+                else if (editTextSymbol == "2") {
+                    res = editText1.toInt() - editText2.toInt()
+                    binding.tvResult.text = res.toString()
+                }
+                else if (editTextSymbol == "3") {
+                    res = editText1.toInt() * editText2.toInt()
+                    binding.tvResult.text = res.toString()
+                }
+                else if (editTextSymbol == "4") {
+                    res = editText1.toInt() / editText2.toInt()
+                    binding.tvResult.text = res.toString()
+                }
+                else {
+                    binding.tvResult.text = "Wrong symbol"
+                }
             }
         }
-
-
-        // repository test
     }
 }
