60dc7a612b4e0a4d4a450ba077e9e5c155f96f7a
main <zheksinbi@mail.ru>
2025-02-27 19:59:01 +0500

    ksilofon

--- app/src/main/java/com/example/applications/MainActivity.kt
+++ app/src/main/java/com/example/applications/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.applications
 
+import android.media.MediaPlayer
 import android.os.Bundle
+import android.provider.MediaStore.Audio.Media
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,16 +18,40 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        val mediaDo = MediaPlayer.create(this, R.raw.do_1)
+        val mediaRe = MediaPlayer.create(this, R.raw.re)
+        val mediaMi = MediaPlayer.create(this, R.raw.mi)
+        val mediaFa = MediaPlayer.create(this, R.raw.fa)
+        val mediaSol = MediaPlayer.create(this, R.raw.sol)
+        val mediaLya = MediaPlayer.create(this, R.raw.lya)
+        val mediaSi = MediaPlayer.create(this, R.raw.si)
+        val mediaDo2 = MediaPlayer.create(this, R.raw.do_2)
 
-        var flag = true
-        binding.button.setOnClickListener {
-            if (flag) {
-                binding.imageView.setImageResource(R.mipmap.red_crystal)
-                flag = false
-            } else {
-                binding.imageView.setImageResource(R.mipmap.blue_crystal)
-                flag = true
-            }
+
+        binding.bntDo.setOnClickListener {
+            mediaDo.start()
         }
+        binding.bntRe.setOnClickListener {
+            mediaRe.start()
+        }
+        binding.bntMi.setOnClickListener {
+            mediaMi.start()
+        }
+        binding.bntFa.setOnClickListener {
+            mediaFa.start()
+        }
+        binding.bntSol.setOnClickListener {
+            mediaSol.start()
+        }
+        binding.bntLya.setOnClickListener {
+            mediaLya.start()
+        }
+        binding.bntSi.setOnClickListener {
+            mediaSi.start()
+        }
+        binding.bntDo2.setOnClickListener {
+            mediaDo2.start()
+        }
+
     }
 }
